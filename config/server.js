7f667bd73a6eabef4c32ce953a901daee700b0df
Khuong Duy <ly.khuong1996@gmail.com>
2022-11-21 22:42:42 +0700

    deploy

--- config/server.js
+++ config/server.js
@@ -1,7 +1,6 @@
 module.exports = ({ env }) => ({
   host: env("HOST", "0.0.0.0"),
   port: env.int("PORT", 1337),
-  url: "https://ktcode-cms.herokuapp.com",
   app: {
     keys: env.array("APP_KEYS"),
   },
