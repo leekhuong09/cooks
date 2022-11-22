5a4a2c702ab563e8c8a482a92a9702f159c3dd9d
Khuong Duy <ly.khuong1996@gmail.com>
2022-11-22 21:18:37 +0700

    change db

--- config/database.js
+++ config/database.js
@@ -1,11 +1,24 @@
-const path = require('path');
+const path = require("path");
 
 module.exports = ({ env }) => ({
   connection: {
-    client: 'sqlite',
+    client: "postgres",
     connection: {
-      filename: path.join(__dirname, '..', env('DATABASE_FILENAME', '.tmp/data.db')),
+      // filename: path.join(__dirname, '..', env('DATABASE_FILENAME', '.tmp/data.db')),
+      host: env("DATABASE_HOST", "ec2-54-86-214-124.compute-1.amazonaws.com"),
+      port: env.int("DATABASE_PORT", 5432),
+      database: env("DATABASE_NAME", "dbos6f82enl4jf"),
+      user: env("DATABASE_USERNAME", "woxmcfvauqocbl"),
+      password: env(
+        "DATABASE_PASSWORD",
+        "ed7552fbe99bd185b7af0567a972f7c3882b28cda5b3b10c2d5370539ee2bf2f"
+      ),
+      schema: env("DATABASE_SCHEMA", "public"), // Not required
+      ssl: {
+        rejectUnauthorized: env.bool("DATABASE_SSL_SELF", false),
+      },
     },
-    useNullAsDefault: true,
+    // useNullAsDefault: true,
+    debug: false,
   },
 });
