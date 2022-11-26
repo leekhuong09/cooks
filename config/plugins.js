5aae76b1ae7ffcb9124bad739b40029874c3eb53
Khuong Duy <ly.khuong1996@gmail.com>
2022-11-26 14:28:46 +0700

    change relation

--- config/plugins.js
+++ config/plugins.js
@@ -10,4 +10,18 @@ module.exports = ({ env }) => ({
       },
     },
   },
+  // "vercel-deploy": {
+  //   enabled: true,
+  //   // config: {
+  //   //   deployHook:
+  //   //     "https://api.vercel.com/v1/integrations/deploy/prj_OfY9vfxFklzc6Y7IpTxDClNvfk5z/LZDOP00lmh",
+  //   //   apiToken: "6DGnWuS1Y5XT2toosrpIoTvJ",
+  //   //   appFilter: "cooks",
+  //   //   teamFilter: "khuongldk",
+  //   //   roles: ["strapi-super-admin", "strapi-editor", "strapi-author"],
+  //   // },
+  // },
+  seo: {
+    enabled: true,
+  },
 });
