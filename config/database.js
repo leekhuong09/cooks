7d0c280a8dcf81721a9ff815ed386e01aac5e2d3
khuongduy <ly.khuong1996@gmail.com>
2022-12-15 10:29:52 +0700

    use neon.tech

--- config/database.js
+++ config/database.js
@@ -5,14 +5,14 @@ module.exports = ({ env }) => ({
     client: "postgres",
     connection: {
       // filename: path.join(__dirname, '..', env('DATABASE_FILENAME', '.tmp/data.db')),
-      host: env("DATABASE_HOST", "ec2-54-86-214-124.compute-1.amazonaws.com"),
-      port: env.int("DATABASE_PORT", 5432),
-      database: env("DATABASE_NAME", "dbos6f82enl4jf"),
-      user: env("DATABASE_USERNAME", "woxmcfvauqocbl"),
-      password: env(
-        "DATABASE_PASSWORD",
-        "ed7552fbe99bd185b7af0567a972f7c3882b28cda5b3b10c2d5370539ee2bf2f"
+      host: env(
+        "DATABASE_HOST",
+        "ep-young-block-056015.us-east-2.aws.neon.tech"
       ),
+      port: env.int("DATABASE_PORT", 5432),
+      database: env("DATABASE_NAME", "naumongi"),
+      user: env("DATABASE_USERNAME", "leekhuong09"),
+      password: env("DATABASE_PASSWORD", "mUCv4sanKi2h"),
       schema: env("DATABASE_SCHEMA", "public"), // Not required
       ssl: {
         rejectUnauthorized: env.bool("DATABASE_SSL_SELF", false),
