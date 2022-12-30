fde9cd11097006b9199628429e3cf9130bd45b71
Khuong Duy <ly.khuong1996@gmail.com>
2022-12-30 21:16:41 +0700

    .

--- config/database.js
+++ config/database.js
@@ -12,7 +12,7 @@ module.exports = ({ env }) => ({
       port: env.int("DATABASE_PORT", 5432),
       database: env("DATABASE_NAME", "naumongi"),
       user: env("DATABASE_USERNAME", "leekhuong09"),
-      password: env("DATABASE_PASSWORD", "mUCv4sanKi2h"),
+      password: env("DATABASE_PASSWORD", "1Gzji9gmXYpk"),
       schema: env("DATABASE_SCHEMA", "public"), // Not required
       ssl: {
         rejectUnauthorized: env.bool("DATABASE_SSL_SELF", false),
